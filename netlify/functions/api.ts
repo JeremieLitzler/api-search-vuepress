8393980336d78a186317521e5f42afbce8047eb3
Jérémie LITZLER <2410556+JeremieLitzler@users.noreply.github.com>
2023-07-06 08:44:11 +0200

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -14,11 +14,4 @@ api.use(express.json());
 /* Routes */
 api.use(`/${API_BASE}`, searchRoutes);
 
-/* Server setup */
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () =>
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}/${API_BASE}`)
-  );
-}
-
 export const handler = serverless(api);
