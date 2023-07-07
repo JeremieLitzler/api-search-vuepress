512316c1e4d188b500b18bff5aab2e84e30a903b
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-07-07 06:47:05 +0200

    fix: update index.html to index.php + adjust api base

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import searchRoutes from "./routes/search.routes.js";
+import searchRoutes from './routes/search.routes.js';
 
 const app = express();
 const port = 3000;
-const API_BASE = "api/search/"
+const API_BASE = 'api/';
 
 /* Global middlewares */
 app.use(cors());
@@ -15,9 +15,11 @@ app.use(express.json());
 app.use(`/${API_BASE}`, searchRoutes);
 
 /* Server setup */
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () =>
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}/${API_BASE}`)
+    console.log(
+      `⚡️[server]: Server is running at http://localhost:${port}/${API_BASE}`,
+    ),
   );
 }
 
